913d2d35aa2aa13309e3e28979f56e60f679cb39
iyadhbf <iyadhbelfetni975@gmail.com>
2024-11-28 22:41:40 +0100

    dashboard component

--- client/src/components/SalesPage.jsx
+++ client/src/components/SalesPage.jsx
@@ -90,6 +90,7 @@ const SalesPage = () => {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
+      
       <div className="flex-1 flex flex-col bg-gray-50">
         <ToastContainer />
 
